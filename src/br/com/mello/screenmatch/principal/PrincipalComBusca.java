e103141539f73a4d668b7faf685d229fa6c8e216
Gustavo Mello <oliver7mello@gmail.com>
2024-10-22 02:07:58 -0300

    Aprendendo usar FileWriter criando JSONs

--- src/br/com/mello/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/mello/screenmatch/principal/PrincipalComBusca.java
@@ -1,6 +1,7 @@
 package br.com.mello.screenmatch.principal;
 
 import br.com.mello.screenmatch.excecoes.ExcecaoDeConversaoDeAno;
+import br.com.mello.screenmatch.modelos.Filme;
 import br.com.mello.screenmatch.modelos.Titulo;
 import br.com.mello.screenmatch.modelos.TituloOmdb;
 import com.google.gson.FieldNamingPolicy;
@@ -8,52 +9,74 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.sun.net.httpserver.HttpServer;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
         Scanner entrada = new Scanner(System.in);
+        List<Titulo> historicoPesquisa = new ArrayList<>();
         Gson gson = new GsonBuilder()
                 .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                .setPrettyPrinting()
                 .create();
+        String filme = "";
+        while (!filme.equals("sair")) {
+            System.out.print("Digite o nome do filme qual deseja pesquisar: ");
+            filme = entrada.nextLine().replace(" ", "%20");
 
-        System.out.print("Digite o nome do filme qual deseja pesquisar: ");
-        String filme = entrada.nextLine().replace(" ", "%20");
-        //System.out.println("String pesquisa: " + filme);
-        String API_KEY = "1a6bcfd8"; // ainda não sei usar .env em Java XDDDD
+            if (filme.equals("sair")) {
+                break;
+            }
 
-        try {
-            HttpClient client = HttpClient.newHttpClient();
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(String.format("http://www.omdbapi.com/?apikey=%s&t=%s", API_KEY, filme)))
-                    .build();
+            //System.out.println("String pesquisa: " + filme);
+            String API_KEY = "1a6bcfd8"; // ainda não sei usar .env em Java XDDDD
 
-            HttpResponse<String> response = client
-                    .send(request, HttpResponse.BodyHandlers.ofString());
 
-            String jsonResponse = response.body();
-            //System.out.println("Resposta da API: " + jsonResponse);
+            try {
+                HttpClient client = HttpClient.newHttpClient();
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(URI.create(String.format("http://www.omdbapi.com/?apikey=%s&t=%s", API_KEY, filme)))
+                        .build();
 
-            TituloOmdb tituloResultado = gson.fromJson(jsonResponse, TituloOmdb.class);
-            Titulo tituloResultadoFinal = new Titulo(tituloResultado);
-            System.out.println(tituloResultadoFinal);
-        } catch (NumberFormatException e) {
-            System.out.println("Ocorreu um erro.");
-            System.out.println(e.getMessage());
-        } catch (IllegalArgumentException e) {
-            System.out.println("Ocorreu um erro.\n Há um problema no endereço de pesquisa, verifique o nome do filme digitado.");
-            System.out.println(e.getMessage());
-        } catch (ExcecaoDeConversaoDeAno e) {
-            System.out.println(e.getMessage());
+                HttpResponse<String> response = client
+                        .send(request, HttpResponse.BodyHandlers.ofString());
+
+                String jsonResponse = response.body();
+                //System.out.println("Resposta da API: " + jsonResponse);
+
+                TituloOmdb tituloResultado = gson.fromJson(jsonResponse, TituloOmdb.class);
+                Titulo tituloResultadoFinal = new Titulo(tituloResultado);
+                System.out.println(tituloResultadoFinal);
+                historicoPesquisa.add(tituloResultadoFinal);
+
+            } catch (NumberFormatException e) {
+                System.out.println("Ocorreu um erro.");
+                System.out.println(e.getMessage());
+            } catch (IllegalArgumentException e) {
+                System.out.println("Ocorreu um erro.\n Há um problema no endereço de pesquisa, verifique o nome do filme digitado.");
+                System.out.println(e.getMessage());
+            } catch (ExcecaoDeConversaoDeAno e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        if (!historicoPesquisa.isEmpty()) {
+            System.out.println("Pesquisados: " + historicoPesquisa);
+
+            FileWriter escrita = new FileWriter("filmes.json");
+            escrita.write(gson.toJson(historicoPesquisa));
+            escrita.close();
         }
 
         System.out.println("Encerrando programa...");
-
     }
 
 }
